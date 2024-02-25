64ad320fedb5073d5a644f46ac06b18f6c913c9d
Jeremy Belton <jbelton@gmail.com>
2024-02-24 19:03:00 -0500

    second commit

---
+++ module/helpers/config.mjs
@@ -0,0 +1,23 @@
+export const STRIKE = {};
+
+/**
+ * The set of Ability Scores used within the system.
+ * @type {Object}
+ */
+STRIKE.abilities = {
+  str: 'STRIKE.Ability.Str.long',
+  dex: 'STRIKE.Ability.Dex.long',
+  con: 'STRIKE.Ability.Con.long',
+  int: 'STRIKE.Ability.Int.long',
+  wis: 'STRIKE.Ability.Wis.long',
+  cha: 'STRIKE.Ability.Cha.long',
+};
+
+STRIKE.abilityAbbreviations = {
+  str: 'STRIKE.Ability.Str.abbr',
+  dex: 'STRIKE.Ability.Dex.abbr',
+  con: 'STRIKE.Ability.Con.abbr',
+  int: 'STRIKE.Ability.Int.abbr',
+  wis: 'STRIKE.Ability.Wis.abbr',
+  cha: 'STRIKE.Ability.Cha.abbr',
+};
